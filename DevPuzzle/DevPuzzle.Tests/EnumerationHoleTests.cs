f7ad524aecad4dd4e5e37ede16f4f350b607c680
fomaa <roman.nofenko@gmail.com>
2015-09-11 15:09:55 -0400

    new test

---
+++ DevPuzzle/DevPuzzle.Tests/EnumerationHoleTests.cs
@@ -0,0 +1,14 @@
+using NUnit.Framework;
+
+namespace DevPuzzle.Tests
+{
+    [TestFixture]
+    public class EnumerationHoleTests
+    {
+        [Test]
+        public void t()
+        {
+            var source = new[] {1, 3, 4, 5, 6, 9, 10};
+        }
+    }
+}
