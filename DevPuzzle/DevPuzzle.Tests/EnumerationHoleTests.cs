9d63b8c97222d237fe900e491308df2b1a004de3
mazbros <mazbros@gmail.com>
2015-09-17 10:12:41 -0400

    Implemented EnumarationHoleFinder

--- DevPuzzle/DevPuzzle.Tests/EnumerationHoleTests.cs
+++ DevPuzzle/DevPuzzle.Tests/EnumerationHoleTests.cs
@@ -9,23 +9,36 @@ namespace DevPuzzle.Tests
     {
         private BubbleSort _sort;
         private EnumerationHoleFiller _filler;
+        private EnumerationHoleFinder _finder;
 
         [SetUp]
         public void SetUp()
         {
             _sort = new BubbleSort();
             _filler = new EnumerationHoleFiller();
+            _finder = new EnumerationHoleFinder();
         }
 
         [Test]
-        public void Absent_elements_should_be_in_result()
+        public void Absent_elements_should_be_displayed_in_result()
         {
-            var source = new[] {6, 10, 5, 1 };
+            var source = new[] { 6, 10, 5, 1, 12 };
+
+            var sortedSource = _sort.Sort(source.ToList()).ToArray();
+            var foundResults = _finder.Find(sortedSource);
+
+            Ass.Equal(new[] { "2-4", "7-9", "11" }, foundResults);
+        }
+
+        [Test]
+        public void Absent_elements_should_be_included_in_result()
+        {
+            var source = new[] { 6, 10, 5, 1 };
 
             var sortedSource = _sort.Sort(source.ToList()).ToArray();
             var filledSource = _filler.Fill(sortedSource);
 
-            Ass.Equal(new[] { "2-4", "7-9" }, filledSource);
+            Ass.Equal(new[] { 1, 2, 3, 4, 5, 6, 7 ,8, 9, 10 }, filledSource);
         }
     }
 }
---
+++ DevPuzzle/DevPuzzle/EnumerationHoleFinder.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DevPuzzle
+{
+    public class EnumerationHoleFinder
+    {
+        public string[] Find(int[] sortedSource)
+        {
+            var foundHoles = new string[0];
+            
+            var counter = 0;
+            
+
+            for (var i = 0; i < sortedSource.Length; i++)
+            {
+                var result = new List<int>();
+                var tempStr = string.Empty;
+
+                if (i >= sortedSource.Length - 1) continue;
+
+                if (sortedSource[i + 1] - sortedSource[i] <= 1) continue;
+
+                var holeSize = (sortedSource[i + 1] - sortedSource[i]) - 1;
+
+                result.Add(sortedSource[i] + 1);
+
+                if (holeSize > 1)
+                {
+                    result.Add(sortedSource[i] + holeSize);
+                }
+
+                tempStr = result.Aggregate(tempStr, (current, t) => current + $"{t}-");
+                Array.Resize(ref foundHoles, foundHoles.Length + 1);
+
+                foundHoles[counter] = tempStr.Substring(0, tempStr.Length - 1);
+                counter++;
+            }
+
+            return foundHoles;
+        }
+    }
+}
