289ce22b9ae9725eafeb42a87f5713cb56dc24f1
Roman Nofenko <roman.nofenko@kohls.com>
2020-02-24 17:38:15 -0600

    burrows-wheeler task

---
+++ coursera/burrows/CircularSuffixArray.java
@@ -0,0 +1,20 @@
+/* *****************************************************************************
+ *  Name:
+ *  Date:
+ *  Description:
+ **************************************************************************** */
+
+public class CircularSuffixArray {
+    public CircularSuffixArray(String s) {
+
+    }
+
+    public int length() {
+        return 0;
+    }
+
+    // returns index of ith sorted suffix
+    public int index(int i) {
+        return 0;
+    }
+}
