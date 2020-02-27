bcfdbce13c41541be997ac8b47bd117f0b9827ef
Roman Nofenko <roman.nofenko@kohls.com>
2020-02-27 17:08:22 -0600

    p

--- coursera/burrows/MoveToFront.java
+++ coursera/burrows/MoveToFront.java
@@ -4,10 +4,18 @@
  *  Description:
  **************************************************************************** */
 
+import edu.princeton.cs.algs4.BinaryStdIn;
+import edu.princeton.cs.algs4.BinaryStdOut;
+import edu.princeton.cs.algs4.StdOut;
+
 public class MoveToFront {
     // apply move-to-front encoding, reading from standard input and writing to standard output
     public static void encode() {
-        
+        while (!BinaryStdIn.isEmpty()) {
+            char c = BinaryStdIn.readChar();
+            BinaryStdOut.write(c);
+        }
+        BinaryStdIn.close();
     }
 
     // apply move-to-front decoding, reading from standard input and writing to standard output
@@ -18,6 +26,14 @@ public class MoveToFront {
     // if args[0] is "-", apply move-to-front encoding
     // if args[0] is "+", apply move-to-front decoding
     public static void main(String[] args) {
-
+        if (args.length == 0) {
+            StdOut.println("No args. User - or +");
+        } else {
+            if (args[0].equals("+")) {
+                decode();
+            } else {
+                encode();
+            }
+        }
     }
 }
